c752e88f952bb41399962d0cabd3a6926d3da6a0
AlexsanderKim <alexsanderkimchoi08@gmail.com>
2025-05-24 11:42:35 -0600

    Adivinar Numeros Aleatorios

---
+++ AdivinarNumeroAleatorio/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Random generador = new Random ();
+int aleatorio = generador.Next (0,51);
+Console.WriteLine ("adivine el numero generado aleatoriamente");
+int numRandom;
+numRandom = Convert.ToInt32 (Console.ReadLine());
+while (numRandom != aleatorio)
+{
+    if (numRandom < aleatorio )
+            {
+                Console.WriteLine ("el numero que elegiste es menor");
+            }
+    else if (numRandom > aleatorio)
+            {
+                Console.WriteLine ("el numero que elegiste es mayor");
+            }
+            numRandom = Convert.ToInt32 (Console.ReadLine ());
+}
+Console.WriteLine ("Felicidades el numero es " + aleatorio);
